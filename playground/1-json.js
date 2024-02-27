e5ffaaf9672296f270b7bac6ab8ced975d4ab462
Khoa Hoang <khoahoang1698@gmail.com>
2024-02-27 15:59:20 -0800

    update

---
+++ playground/1-json.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Moliday'
+// }
+
+
+// const bookJSON = JSON.stringify(book);
+
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+
+// const data = JSON.parse(dataJSON);
+
+// console.log(data.title);
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+data.name = 'Khoa';
+data.planet = 'Mars';
+data.age = '26';
+
+console.log(data);
+
+
