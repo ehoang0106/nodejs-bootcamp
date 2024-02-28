5dd7b0551633d8c451d2149618f9ad85902c4430
Khoa Hoang <khoahoang1698@gmail.com>
2024-02-27 16:23:23 -0800

    update

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,17 +7,30 @@ const getNotes = function () {
 const addNote = function (title, body) {
     const notes = loadNotes(); //this is an array
 
-    notes.push({
-        title: title,
-        body: body
-    })
-    
-    saveNotes(notes);
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title;
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+
+        saveNotes(notes);
+        console.log('New note added');
+    }
+    else {
+        console.log('Note title taken!');
+    }
+
+
 }
 
 
 const saveNotes = function (notes) {
-
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
 const loadNotes = function () {
