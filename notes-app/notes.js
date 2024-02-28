b5ec940c9b4e75fbc8b0f891789cf6796cdaff5f
Khoa Hoang <khoahoang1698@gmail.com>
2024-02-28 12:35:13 -0800

    update

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -10,6 +10,8 @@ const addNote =  (title, body) => {
 
     const duplicateNote = notes.find((note) => note.title === title);
 
+    debugger
+
     if (!duplicateNote) {
         notes.push({
             title: title,
