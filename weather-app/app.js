4af3ba57505e1c12ab71aa2771e4902319f345e7
Khoa Hoang <khoahoang1698@gmail.com>
2024-02-29 09:24:25 -0800

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,32 @@
-console.log('Starting...');
+const request = require('postman-request')
+const API = '82ce116fef7461f1900a7fcde78ad5d1'
+const url = 'http://api.weatherstack.com/current?access_key=f416a51140952488e7a617887f8b1b92&query=92840&units=f'
 
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
+//convert Kelvin to Fahrenheit
+const fahrenheit = (k) => Math.floor((k-273.15)*9/5+32);
 
+//convert Celsius to Fahrenheit
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0)
-
-
-
+const f = (c) => Math.floor((c) * 9/5) + 32;
 
 
 
+request({url: url, json: true}, (error, response) => {
+    const data = response.body;
+    
+    const cityName = data.location.name;
+    const region = data.location.region;
+    const localTime = data.location.localtime;
+    
+    const currentTemp = data.current.temperature;
+    const weatherDescription = data.current.weather_descriptions
+    
+    console.log('Region: ' + region);
+    console.log('Location: ' + cityName);
+    console.log('Local time: ' + localTime);
+    console.log('Current Temperature: ' + currentTemp + '°F - ' + weatherDescription);
 
-console.log('Stopping...');
+    
 
-
-
-
+})
---
+++ weather-app/app1.js
@@ -0,0 +1,44 @@
+
+const request = require('postman-request');
+
+const API = '82ce116fef7461f1900a7fcde78ad5d1';
+
+const geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=Orange,CA,1&appid=82ce116fef7461f1900a7fcde78ad5d1';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=33.7872568&lon=-117.850308&units=imperial&appid=82ce116fef7461f1900a7fcde78ad5d1';
+
+const getGeo = () => {
+    request({url: geoUrl, json: true}, (error, response) => {
+        const data = response.body[0];
+        const lat = data.lat;
+        const lon = data.lon;    
+        
+        
+        console.log(lat);
+        console.log(lon);
+    })
+}
+//getGeo();
+
+const getWeatherData = () => {
+    request({url: weatherUrl, json: true}, (error, response) => {
+        const data = response.body;
+        const weatherMain = data.weather[0].main;
+        const weatherDescription = data.weather[0].description;
+        const currentTemp = data.main.temp;
+        const tempMin = data.main.temp_min;
+        const tempMax = data.main.temp_max;
+        const location = data.name;
+        
+        console.log('Weather in ' + location + ':');
+        console.log(weatherMain + ' - ' + weatherDescription);
+        console.log('Current Tempature: ' + currentTemp);
+        console.log('Maximum Tempature: ' + tempMax + '\n' + 'Minimum Tempature: ' + tempMin);
+        
+    })
+};
+
+//getWeatherData();
+
+request({url: weatherUrl, json: true}, (error, response) => {
+    console.log(error);
+})
